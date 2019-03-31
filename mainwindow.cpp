99dcf5e244d08c858dcba065f8a22c78946a29db
csy's dell <995816158@qq.com>
2019-03-31 11:45:31 +0800

    增加了部分测试模块

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,8 +6,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    CPUs* mainCPU = new CPUs();
-
+    CPUs cp;
+    cp.setRegValue(MicroCom::ax,127);
+    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
+    cp.setRegValue(MicroCom::ax,high,7);
+    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
+    cp.setRegValue(MicroCom::ax,-1);
+    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
+    cp.setRegValue(MicroCom::ah,-1);
+    qDebug()<<"ah="<<cp.getRegValue(MicroCom::ah);
 }
 
 MainWindow::~MainWindow()

e24158ccf887fccf91aa3a394a7f6e72f61095f0
csy's dell <995816158@qq.com>
2019-03-31 14:23:01 +0800

    又改了代码测试。。。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,14 +7,16 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     CPUs cp;
-    cp.setRegValue(MicroCom::ax,127);
-    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
-    cp.setRegValue(MicroCom::ax,high,7);
-    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
-    cp.setRegValue(MicroCom::ax,-1);
-    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
-    cp.setRegValue(MicroCom::ah,-1);
-    qDebug()<<"ah="<<cp.getRegValue(MicroCom::ah);
+//    cp.setRegValue(MicroCom::ax,127);
+//    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
+//    cp.setRegValue(MicroCom::ax,high,7);
+//    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
+//    cp.setRegValue(MicroCom::ax,-1);
+//    qDebug()<<"ax="<<cp.getRegValue(MicroCom::ax);
+//    cp.setRegValue(MicroCom::ah,-1);
+//    qDebug()<<"ah="<<cp.getRegValue(MicroCom::ah);
+    cp.emitReset();
+    qDebug()<<"cs="<<cp.getRegValue(MicroCom::cs);
 }
 
 MainWindow::~MainWindow()

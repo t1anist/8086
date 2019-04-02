b72d406e90c153c35045dbf251a75a13da618220
HonorCsy <995816158@qq.com>
2019-04-02 21:46:19 +0800

    ·将CPUs类中的toCompForm函数和toTrueForm函数移到了基类中

--- hardwares.cpp
+++ hardwares.cpp
@@ -25,3 +25,50 @@ void Hardwares::setValueByPos(unsigned short &value, short pos, MicroCom::Regs r
     value &= temp;
     value |= flag;
 }
+
+/****************************************************
+ - Function：True Form to Complement Form
+ - Description：The default length of the regs is 16-bit
+ - Calls：
+ - Called By：
+ - Input：[value(true form), MicroCom::RegsLen]
+ - Output：
+ - Return：value(complement form)[16-bit unsigned short]
+*****************************************************/
+unsigned short Hardwares::toCompForm(short value, MicroCom::RegsLen len){
+    unsigned char temp = 0;
+    //16-bit register
+    if(len==MicroCom::dbyte){
+        return static_cast<unsigned short>(value);
+    }
+    //8-bit register
+    else{
+        temp = static_cast<unsigned char>(value);
+    }
+    return static_cast<unsigned short>(temp);
+}
+
+/****************************************************
+ - Function：complement form to signed true form
+ - Description：The default length of the regs is 16-bit
+ - Calls：
+ - Called By：
+ - Input：[value(complement form), MicroCom::RegsLen]
+ - Output：
+ - Return：a signed short number in true form
+*****************************************************/
+short Hardwares::toTrueForm(unsigned short value, MicroCom::RegsLen len){
+    unsigned char temp = 0;
+    short rst=0;
+    //16-bit
+    if(len==MicroCom::dbyte){
+        rst = static_cast<short>(value);
+    }
+    else{
+        value &= 0x00ff;
+        temp = static_cast<unsigned char>(value);
+        rst = static_cast<char>(temp);
+        rst = static_cast<short>(rst);
+    }
+    return rst;
+}
--- hardwares.h
+++ hardwares.h
@@ -14,6 +14,11 @@ public:
     explicit Hardwares(QWidget *parent = nullptr);
     virtual bool setVoltage(MicroCom::Pins pin, Voltage pinVol)=0;
     void setValueByPos(unsigned short &value, short pos, MicroCom::Regs reg, Voltage biValue);
+
+    //True Form(原码) to Complement Form(补码)
+    unsigned short toCompForm(short value, MicroCom::RegsLen len = MicroCom::dbyte);
+    //Complement Form to True Form 默认长度为16位
+    short toTrueForm(unsigned short value, MicroCom::RegsLen len = MicroCom::dbyte);
 signals:
 
 public slots:

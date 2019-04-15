dbc98e00056c760cb8b790a983bc352d00d16a84
HonorCsy <995816158@qq.com>
2019-04-15 21:38:28 +0800

    ·删掉了toCompForm和toTrueForm函数

--- Hardwares.h
+++ Hardwares.h
@@ -16,15 +16,12 @@ public:
     explicit Hardwares(QWidget *parent = nullptr);
     virtual void handlePinVolChanges(MicroCom::Pins pin, Voltage value)=0;
     virtual Voltage getPinVoltage(MicroCom::Pins pin)=0;
+
     //set hardware's name
     void setHardwareName(QString hdName);
     //get hardware's name
     QString getHardwareName();
 
-    //True Form to Complement Form
-    unsigned short toCompForm(short value, MicroCom::RegsLen len = MicroCom::dbyte);
-    //Complement Form to True Form
-    short toTrueForm(unsigned short value, MicroCom::RegsLen len = MicroCom::dbyte);
     //非阻塞延时函数
     void delaymsec(int msec);
 

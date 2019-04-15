69509eafea5b28d8e688b97210d6a6d9a376710a
HonorCsy <995816158@qq.com>
2019-04-15 09:54:43 +0800

    ·将Buffers244和Buffers245两个类合并为Buffers类

---
+++ Buffers.cpp
@@ -0,0 +1,6 @@
+#include "Buffers.h"
+
+Buffers::Buffers()
+{
+
+}
---
+++ Buffers.h
@@ -0,0 +1,11 @@
+#ifndef BUFFERS_H
+#define BUFFERS_H
+
+
+class Buffers : public Hardwares
+{
+public:
+    Buffers();
+};
+
+#endif // BUFFERS_H

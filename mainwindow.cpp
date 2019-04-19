1eb0dc48ef0a848820de5dcce5a0977e22ea27a1
HonorCsy <995816158@qq.com>
2019-04-19 22:38:07 +0800

    完善指令函数 ·仍按照既定的思路往下写指令函数（函数重载） ·可以参考机器码

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    cp1 = new CPUs("8086CPU");
+    cp = new CPUs("8086CPU");
     //三个锁存器
     la1 = new Latchs("74LS373_1");
     la2 = new Latchs("74LS373_2");
@@ -25,56 +25,56 @@ MainWindow::MainWindow(QWidget *parent) :
     /** 将CPU的地址线与74LS373锁存器的输入端相连 **/
 
     //将A0~A7与锁存器la1的DI0~DI7相连
-    link(cp1,MicroCom::CP_AD0,la1,MicroCom::LA_DI0);
-    link(cp1,MicroCom::CP_AD1,la1,MicroCom::LA_DI1);
-    link(cp1,MicroCom::CP_AD2,la1,MicroCom::LA_DI2);
-    link(cp1,MicroCom::CP_AD3,la1,MicroCom::LA_DI3);
-    link(cp1,MicroCom::CP_AD4,la1,MicroCom::LA_DI4);
-    link(cp1,MicroCom::CP_AD5,la1,MicroCom::LA_DI5);
-    link(cp1,MicroCom::CP_AD6,la1,MicroCom::LA_DI6);
-    link(cp1,MicroCom::CP_AD7,la1,MicroCom::LA_DI7);
-    link(cp1,MicroCom::CP_ALE,la1,MicroCom::LA_G);
+    link(cp,MicroCom::CP_AD0,la1,MicroCom::LA_DI0);
+    link(cp,MicroCom::CP_AD1,la1,MicroCom::LA_DI1);
+    link(cp,MicroCom::CP_AD2,la1,MicroCom::LA_DI2);
+    link(cp,MicroCom::CP_AD3,la1,MicroCom::LA_DI3);
+    link(cp,MicroCom::CP_AD4,la1,MicroCom::LA_DI4);
+    link(cp,MicroCom::CP_AD5,la1,MicroCom::LA_DI5);
+    link(cp,MicroCom::CP_AD6,la1,MicroCom::LA_DI6);
+    link(cp,MicroCom::CP_AD7,la1,MicroCom::LA_DI7);
+    link(cp,MicroCom::CP_ALE,la1,MicroCom::LA_G);
     //将A8~A15与锁存器la2的DI0~DI7相连
-    link(cp1,MicroCom::CP_AD8,la2,MicroCom::LA_DI0);
-    link(cp1,MicroCom::CP_AD9,la2,MicroCom::LA_DI1);
-    link(cp1,MicroCom::CP_AD10,la2,MicroCom::LA_DI2);
-    link(cp1,MicroCom::CP_AD11,la2,MicroCom::LA_DI3);
-    link(cp1,MicroCom::CP_AD12,la2,MicroCom::LA_DI4);
-    link(cp1,MicroCom::CP_AD13,la2,MicroCom::LA_DI5);
-    link(cp1,MicroCom::CP_AD14,la2,MicroCom::LA_DI6);
-    link(cp1,MicroCom::CP_AD15,la2,MicroCom::LA_DI7);
-    link(cp1,MicroCom::CP_ALE,la2,MicroCom::LA_G);
+    link(cp,MicroCom::CP_AD8,la2,MicroCom::LA_DI0);
+    link(cp,MicroCom::CP_AD9,la2,MicroCom::LA_DI1);
+    link(cp,MicroCom::CP_AD10,la2,MicroCom::LA_DI2);
+    link(cp,MicroCom::CP_AD11,la2,MicroCom::LA_DI3);
+    link(cp,MicroCom::CP_AD12,la2,MicroCom::LA_DI4);
+    link(cp,MicroCom::CP_AD13,la2,MicroCom::LA_DI5);
+    link(cp,MicroCom::CP_AD14,la2,MicroCom::LA_DI6);
+    link(cp,MicroCom::CP_AD15,la2,MicroCom::LA_DI7);
+    link(cp,MicroCom::CP_ALE,la2,MicroCom::LA_G);
     //将A16~A20与锁存器la3的DI0~DI3相连
-    link(cp1,MicroCom::CP_AS16,la3,MicroCom::LA_DI0);
-    link(cp1,MicroCom::CP_AS17,la3,MicroCom::LA_DI1);
-    link(cp1,MicroCom::CP_AS18,la3,MicroCom::LA_DI2);
-    link(cp1,MicroCom::CP_AS19,la3,MicroCom::LA_DI3);
-    link(cp1,MicroCom::CP_bhe,la3,MicroCom::LA_DI4);
-    link(cp1,MicroCom::CP_ALE,la3,MicroCom::LA_G);
+    link(cp,MicroCom::CP_AS16,la3,MicroCom::LA_DI0);
+    link(cp,MicroCom::CP_AS17,la3,MicroCom::LA_DI1);
+    link(cp,MicroCom::CP_AS18,la3,MicroCom::LA_DI2);
+    link(cp,MicroCom::CP_AS19,la3,MicroCom::LA_DI3);
+    link(cp,MicroCom::CP_bhe,la3,MicroCom::LA_DI4);
+    link(cp,MicroCom::CP_ALE,la3,MicroCom::LA_G);
 
     /** 将CPU与缓存器相连 **/
     //将D0~D7与缓冲器bf1的A0~A7相连
-    link(cp1,MicroCom::CP_AD0,bf1,MicroCom::BF5_A0);
-    link(cp1,MicroCom::CP_AD1,bf1,MicroCom::BF5_A1);
-    link(cp1,MicroCom::CP_AD2,bf1,MicroCom::BF5_A2);
-    link(cp1,MicroCom::CP_AD3,bf1,MicroCom::BF5_A3);
-    link(cp1,MicroCom::CP_AD4,bf1,MicroCom::BF5_A4);
-    link(cp1,MicroCom::CP_AD5,bf1,MicroCom::BF5_A5);
-    link(cp1,MicroCom::CP_AD6,bf1,MicroCom::BF5_A6);
-    link(cp1,MicroCom::CP_AD7,bf1,MicroCom::BF5_A7);
-    link(cp1,MicroCom::CP_den,bf1,MicroCom::BF5_g);
-    link(cp1,MicroCom::CP_DTr,bf1,MicroCom::BF5_DIR);
+    link(cp,MicroCom::CP_AD0,bf1,MicroCom::BF5_A0);
+    link(cp,MicroCom::CP_AD1,bf1,MicroCom::BF5_A1);
+    link(cp,MicroCom::CP_AD2,bf1,MicroCom::BF5_A2);
+    link(cp,MicroCom::CP_AD3,bf1,MicroCom::BF5_A3);
+    link(cp,MicroCom::CP_AD4,bf1,MicroCom::BF5_A4);
+    link(cp,MicroCom::CP_AD5,bf1,MicroCom::BF5_A5);
+    link(cp,MicroCom::CP_AD6,bf1,MicroCom::BF5_A6);
+    link(cp,MicroCom::CP_AD7,bf1,MicroCom::BF5_A7);
+    link(cp,MicroCom::CP_den,bf1,MicroCom::BF5_g);
+    link(cp,MicroCom::CP_DTr,bf1,MicroCom::BF5_DIR);
     //将D8~D15与缓冲器bf2的A0~A7相连
-    link(cp1,MicroCom::CP_AD8,bf2,MicroCom::BF5_A0);
-    link(cp1,MicroCom::CP_AD9,bf2,MicroCom::BF5_A1);
-    link(cp1,MicroCom::CP_AD10,bf2,MicroCom::BF5_A2);
-    link(cp1,MicroCom::CP_AD11,bf2,MicroCom::BF5_A3);
-    link(cp1,MicroCom::CP_AD12,bf2,MicroCom::BF5_A4);
-    link(cp1,MicroCom::CP_AD13,bf2,MicroCom::BF5_A5);
-    link(cp1,MicroCom::CP_AD14,bf2,MicroCom::BF5_A6);
-    link(cp1,MicroCom::CP_AD15,bf2,MicroCom::BF5_A7);
-    link(cp1,MicroCom::CP_den,bf2,MicroCom::BF5_g);
-    link(cp1,MicroCom::CP_DTr,bf2,MicroCom::BF5_DIR);
+    link(cp,MicroCom::CP_AD8,bf2,MicroCom::BF5_A0);
+    link(cp,MicroCom::CP_AD9,bf2,MicroCom::BF5_A1);
+    link(cp,MicroCom::CP_AD10,bf2,MicroCom::BF5_A2);
+    link(cp,MicroCom::CP_AD11,bf2,MicroCom::BF5_A3);
+    link(cp,MicroCom::CP_AD12,bf2,MicroCom::BF5_A4);
+    link(cp,MicroCom::CP_AD13,bf2,MicroCom::BF5_A5);
+    link(cp,MicroCom::CP_AD14,bf2,MicroCom::BF5_A6);
+    link(cp,MicroCom::CP_AD15,bf2,MicroCom::BF5_A7);
+    link(cp,MicroCom::CP_den,bf2,MicroCom::BF5_g);
+    link(cp,MicroCom::CP_DTr,bf2,MicroCom::BF5_DIR);
 
     /** 将锁存器与译码器74LS138相连 **/
     link(la1,MicroCom::LA_DO0,de1,MicroCom::DE_A);
@@ -85,13 +85,13 @@ MainWindow::MainWindow(QWidget *parent) :
     link(la1,MicroCom::LA_DO5,de1,MicroCom::DE_C);
     link(la1,MicroCom::LA_DO6,de1,MicroCom::DE_G1);
     link(la1,MicroCom::LA_DO7,nand,MicroCom::LG_I1);
-    link(cp1,MicroCom::CP_Mio,nand,MicroCom::LG_I2);
+    link(cp,MicroCom::CP_Mio,nand,MicroCom::LG_I2);
     link(nand,MicroCom::LG_O,de1,MicroCom::DE_G2a);
 
     /** 将译码器与8255A相连 **/
     link(de1,MicroCom::DE_y6,pp1,MicroCom::PP_cs);
-    link(cp1,MicroCom::CP_wr,pp1,MicroCom::PP_wr);
-    link(cp1,MicroCom::CP_rd,pp1,MicroCom::PP_rd);
+    link(cp,MicroCom::CP_wr,pp1,MicroCom::PP_wr);
+    link(cp,MicroCom::CP_rd,pp1,MicroCom::PP_rd);
 
     /** 将缓冲器与8255A相连 **/
     //数据输出
@@ -114,33 +114,33 @@ MainWindow::MainWindow(QWidget *parent) :
     link(pp1,MicroCom::PP_D7,bf1,MicroCom::BF5_B7);
 
     /** 将缓冲器与8086CPU相连 **/
-    link(bf1,MicroCom::BF5_A0,cp1,MicroCom::CP_AD0);
-    link(bf1,MicroCom::BF5_A1,cp1,MicroCom::CP_AD1);
-    link(bf1,MicroCom::BF5_A2,cp1,MicroCom::CP_AD2);
-    link(bf1,MicroCom::BF5_A3,cp1,MicroCom::CP_AD3);
-    link(bf1,MicroCom::BF5_A4,cp1,MicroCom::CP_AD4);
-    link(bf1,MicroCom::BF5_A5,cp1,MicroCom::CP_AD5);
-    link(bf1,MicroCom::BF5_A6,cp1,MicroCom::CP_AD6);
-    link(bf1,MicroCom::BF5_A7,cp1,MicroCom::CP_AD7);
+    link(bf1,MicroCom::BF5_A0,cp,MicroCom::CP_AD0);
+    link(bf1,MicroCom::BF5_A1,cp,MicroCom::CP_AD1);
+    link(bf1,MicroCom::BF5_A2,cp,MicroCom::CP_AD2);
+    link(bf1,MicroCom::BF5_A3,cp,MicroCom::CP_AD3);
+    link(bf1,MicroCom::BF5_A4,cp,MicroCom::CP_AD4);
+    link(bf1,MicroCom::BF5_A5,cp,MicroCom::CP_AD5);
+    link(bf1,MicroCom::BF5_A6,cp,MicroCom::CP_AD6);
+    link(bf1,MicroCom::BF5_A7,cp,MicroCom::CP_AD7);
 /*
-    cp1->setRegValue(MicroCom::bx,0);
-    cp1->setRegValue(MicroCom::cx,0);
-    cp1->setRegValue(MicroCom::ax,56);
-    qDebug()<<"cp1.ax="<<cp1->getRegValue(MicroCom::ax);
-    cp1->setRegValue(MicroCom::bh,127);
-    qDebug()<<"cp1.bx="<<cp1->getRegValue(MicroCom::bx);
-    cp1->setRegValue(MicroCom::cl,127);
-    qDebug()<<"cp1.cx="<<cp1->getRegValue(MicroCom::cx);*/
+    cp->setRegValue(MicroCom::bx,0);
+    cp->setRegValue(MicroCom::cx,0);
+    cp->setRegValue(MicroCom::ax,56);
+    qDebug()<<"cp.ax="<<cp->getRegValue(MicroCom::ax);
+    cp->setRegValue(MicroCom::bh,127);
+    qDebug()<<"cp.bx="<<cp->getRegValue(MicroCom::bx);
+    cp->setRegValue(MicroCom::cl,127);
+    qDebug()<<"cp.cx="<<cp->getRegValue(MicroCom::cx);*/
 
-    //cp1->setRegValue(MicroCom::al,0xff);
-    mov(cp1,MicroCom::ax,0xff,false);
-    out(cp1,MicroCom::ax,0x76);
+    //cp->setRegValue(MicroCom::al,0xff);
+    mov(MicroCom::ax,0xff);
+    out(MicroCom::ax,0x76);
     //qDebug()<<pp1->getControlRegValue();
-    //cp1->writeBusCycle(0x0076,0x93);
+    //cp->writeBusCycle(0x0076,0x93);
     //qDebug()<<pp1->getControlRegValue();
-    //cp1->readBusCycle(0x0076);
-   //cp1->readBusCycle(0x0070);
-  //cp1->readBusCycle(0x0074);
+    //cp->readBusCycle(0x0076);
+   //cp->readBusCycle(0x0070);
+  //cp->readBusCycle(0x0074);
 
 }
 
@@ -149,8 +149,8 @@ MainWindow::~MainWindow()
     delete ui;
     ui=nullptr;
 
-    delete cp1;
-    cp1=nullptr;
+    delete cp;
+    cp=nullptr;
 
     delete la1;
     la1=nullptr;
@@ -177,97 +177,127 @@ MainWindow::~MainWindow()
     nand=nullptr;
 }
 
-//mov立即数寻址（直接寻址）[value -> reg]
-void MainWindow::mov(CPUs* cp, MicroCom::Regs reg, int value, bool isAddressing){
+/** mov指令 **/
+//mov 立即数寻址（将直接寻址与立即数寻址合并，通过bool isAddressing区分）
+void MainWindow::mov(MicroCom::Regs target, int value, bool isAddressing){
     if(isAddressing){
         value = cp->readBusCycle(value);
     }
-    cp->setRegValue(reg,value);
+    cp->setRegValue(target,value);
     return;
 }
 
-//mov寄存器直接寻址（寄存器间接寻址）
-/*
-void MainWindow::mov(CPUs* cp, MicroCom::Regs regD, MicroCom::Regs regS, bool isAddressing){
-    int rst = 0;
-    if(isAddressing){
-        rst = readAddressing(cp,regS);
-    }
-    else{
-        rst = cp->getRegValue(regS);
-    }
-    cp->setRegValue(regD,rst);
+void MainWindow::mov(int addr, int imValue){
+    cp->writeBusCycle(addr,imValue);
     return;
-}*/
+}
 
-//mov寄存器间接寻址（寄存器间接寻址）
-void MainWindow::mov(CPUs *cp, int addr, MicroCom::Regs regS, bool isAddressing){
-    int rst = 0;
-    if(isAddressing){
-        rst = readAddressing(cp,regS);
-    }
-    else{
-        rst = cp->getRegValue(regS);
-    }
-    cp->writeBusCycle(addr,rst);
+//mov 寄存器寻址
+void MainWindow::mov(MicroCom::Regs target, MicroCom::Regs source){
+    int value = cp->getRegValue(source);
+    cp->setRegValue(target,value);
+    return;
+}
+
+void MainWindow::mov(int addr, MicroCom::Regs source){
+    int value = cp->getRegValue(source);
+    cp->writeBusCycle(addr,value);
+    return;
+}
+
+//mov 寄存器间接/相对寻址 (相对)基址变址寻址
+void MainWindow::mov(MicroCom::Regs target, MicroCom::Regs sBased, int sOffset, MicroCom::Regs sIndexed, MicroCom::Regs sPrefixed){
+    int phyAddr = getPhyAddr(sBased,sOffset,sIndexed,sPrefixed);
+    int value = cp->readBusCycle(phyAddr);
+    cp->setRegValue(target, value);
     return;
 }
 
 
-
-
-
-//read addressing
-int MainWindow::readAddressing(CPUs *cp, MicroCom::Regs based, MicroCom::Regs indexed, MicroCom::Regs prefixed, int count){
-    int phyAddr = 0;
-    if(prefixed != MicroCom::no){
-        phyAddr = cp->getRegValue(prefixed)*16 + cp->getRegValue(based);
-    }
-    else{
-        if(based==MicroCom::bp){
-            phyAddr = cp->getRegValue(MicroCom::ss)*16 + cp->getRegValue(based);
-        }
-        else{
-            phyAddr = cp->getRegValue(MicroCom::ds)*16 + cp->getRegValue(based);
-        }
-    }
-    //偏移量
-    if(count!=0){
-        phyAddr += count;
-    }
-    //变址指针
-    if(indexed != MicroCom::no){
-        phyAddr += cp->getRegValue(indexed);
-    }
-    cp->readBusCycle(phyAddr);
-    return cp->getDataValue(false);
+/** 出入栈指令 **/
+//源操作数为寄存器
+void MainWindow::push(MicroCom::Regs source){
+    int value = cp->getRegValue(source);
+    int newSpValue = cp->getRegValue(MicroCom::sp)-2;
+    int stackAddr = cp->getRegValue(MicroCom::ss)*16 + newSpValue;
+    cp->writeBusCycle(stackAddr,value);
+    cp->setRegValue(MicroCom::sp,newSpValue);
+    return;
 }
 
-//write addressing
-void MainWindow::writeAddressing(CPUs *cp, MicroCom::Regs based, int value, MicroCom::Regs indexed, MicroCom::Regs prefixed, int count){
-    int phyAddr = 0;
-    if(prefixed != MicroCom::no){
-        phyAddr = cp->getRegValue(prefixed)*16 + cp->getRegValue(based);
-    }
-    else{
-        if(based==MicroCom::bp){
-            phyAddr = cp->getRegValue(MicroCom::ss)*16 + cp->getRegValue(based);
-        }
-        else{
-            phyAddr = cp->getRegValue(MicroCom::ds)*16 + cp->getRegValue(based);
-        }
-    }
-    //偏移量
-    if(count!=0){
-        phyAddr += count;
-    }
-    //变址指针
-    if(indexed != MicroCom::no){
-        phyAddr += cp->getRegValue(indexed);
-    }
+//源操作数为存储单元
+//直接赋予地址
+void MainWindow::push(int addr){
+    int value = cp->readBusCycle(addr);
+    int newSpValue = cp->getRegValue(MicroCom::sp)-2;
+    int stackAddr = cp->getRegValue(MicroCom::ss)*16 + newSpValue;
+    cp->writeBusCycle(stackAddr,value);
+    cp->setRegValue(MicroCom::sp,newSpValue);
+    return;
+}
+
+//从寄存器中读取地址
+void MainWindow::push(MicroCom::Regs sBased, int offset, MicroCom::Regs sIndexed,MicroCom::Regs sPrefixed){
+    int phyAddr = getPhyAddr(sBased,offset,sIndexed,sPrefixed);
+    int value = cp->readBusCycle(phyAddr);
+    int newSpValue = cp->getRegValue(MicroCom::sp)-2;
+    int stackAddr = cp->getRegValue(MicroCom::ss)*16 + newSpValue;
+    cp->setRegValue(MicroCom::sp,newSpValue);
+    cp->writeBusCycle(stackAddr,value);
+    return;
+}
+
+
+//目的操作数为寄存器
+void MainWindow::pop(MicroCom::Regs target){
+    int phyAddr = getPhyAddr(MicroCom::sp,0);
+    int value = cp->readBusCycle(phyAddr);
+    cp->setRegValue(target,value);
+    cp->setRegValue(MicroCom::sp,cp->getRegValue(MicroCom::sp)+2);
+    return;
+}
+
+//目的操作数为存储单元
+void MainWindow::pop(int addr){
+    int phyAddr = getPhyAddr(MicroCom::sp,0);
+    int value = cp->readBusCycle(phyAddr);
+    cp->writeBusCycle(addr,value);
+    cp->setRegValue(MicroCom::sp,cp->getRegValue(MicroCom::sp)+2);
+    return;
+}
+
+void MainWindow::pop(MicroCom::Regs tBased, int tOffset, MicroCom::Regs tIndexed, MicroCom::Regs tPrefixed){
+    int stackAddr = getPhyAddr(MicroCom::sp,0);
+    int value = cp->readBusCycle(stackAddr);
+    int phyAddr = getPhyAddr(tBased,tOffset,tIndexed,tPrefixed);
     cp->writeBusCycle(phyAddr,value);
+    cp->setRegValue(MicroCom::sp,cp->getRegValue(MicroCom::sp)+2);
     return;
 }
+
+/** 获取物理地址函数 **/
+int MainWindow::getPhyAddr(MicroCom::Regs based, int offset, MicroCom::Regs indexed, MicroCom::Regs prefixed){
+    int phyAddr = 0;
+    int segAddr = 0;
+    if(prefixed != MicroCom::no){
+        segAddr = cp->getRegValue(prefixed);
+    }
+    else{
+        if(based == MicroCom::bp){
+            segAddr = cp->getRegValue(MicroCom::ss);
+        }
+        else{
+            segAddr = cp->getRegValue(MicroCom::ds);
+        }
+    }
+    phyAddr = segAddr*16 + cp->getRegValue(based) + offset;
+    if(indexed != MicroCom::no){
+        phyAddr += cp->getRegValue(indexed);
+    }
+    return cp->readBusCycle(phyAddr);
+}
+
+
 
 //连线函数
 void MainWindow::link(Hardwares* sender, MicroCom::Pins pinS, Hardwares* receiver, MicroCom::Pins pinR){
@@ -282,7 +312,7 @@ void MainWindow::link(Hardwares* sender, MicroCom::Pins pinS, Hardwares* receive
 
 
 //输入指令IN
-void MainWindow::in(CPUs* cp, MicroCom::Regs reg, int addr){
+void MainWindow::in(MicroCom::Regs reg, int addr){
     int inPortAddr = 0;
     if(addr==-1){   //间接寻址
         inPortAddr = cp->getRegValue(MicroCom::dx);
@@ -294,7 +324,7 @@ void MainWindow::in(CPUs* cp, MicroCom::Regs reg, int addr){
 }
 
 //输出指令OUT
-void MainWindow::out(CPUs* cp, MicroCom::Regs reg, int addr){
+void MainWindow::out(MicroCom::Regs reg, int addr){
     int outPortAddr = 0;
     int value = cp->getRegValue(reg);
     if(addr==-1){   //间接寻址
--- mainwindow.h
+++ mainwindow.h
@@ -23,38 +23,72 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    //mov 立即数寻址
-    //  ·送入寄存器
-    void mov(CPUs *cp, MicroCom::Regs reg, int value, bool isAddressing);
-    //  ·送入存储单元
-    void mov(CPUs *cp, int addr, int imValue);
+    /** mov指令 **/
+    //mov 立即数寻址（将直接寻址与立即数寻址合并，通过bool isAddressing区分）
+    void mov(MicroCom::Regs target, int imValue, bool isAddressing=false);
+    void mov(int addr, int imValue);
 
-    //mov 直接寻址
-    //  存储单元 -> 寄存器
-    void mov(CPUs *cp, MicroCom::Regs regD, int addr);
-    //  存储单元 ->
+    //mov 寄存器寻址
+    void mov(MicroCom::Regs target, MicroCom::Regs source);
+    void mov(int addr, MicroCom::Regs source);
+    //有没有一种简练的方法满足：1、读写mov同名；2、尽可能多地初始化参数；3、辨识度高
+    void mov(MicroCom::Regs tBased, int tOffset, MicroCom::Regs tIndexed,  MicroCom::Regs sPrefixed=MicroCom::no);
 
-    //mov 寄存器直接寻址/间接寻址
-    // memory -> reg
-    void mov(CPUs *cp, MicroCom::Regs regD, MicroCom::Regs regS, bool isAddressing);
-    // memory -> reg
+    //mov 寄存器间接/相对寻址
+    void mov(MicroCom::Regs target, MicroCom::Regs sBased, int sOffset, MicroCom::Regs sIndexed=MicroCom::no, MicroCom::Regs sPrefixed=MicroCom::no);
 
-    void mov(CPUs *cp, int addr, MicroCom::Regs regS, bool isAddressing=false);
-   // void mov(CPUs *cp, MicroCom::Regs basedReg, MicroCom::Regs indexedReg, int count)
-    //寄存器间接寻址（相对寻址）
-    int readAddressing(CPUs *cp, MicroCom::Regs, MicroCom::Regs indexed =MicroCom::no, MicroCom::Regs prefix=MicroCom::no, int count=0);
-    void writeAddressing(CPUs *cp, MicroCom::Regs, int value=0, MicroCom::Regs indexed=MicroCom::no, MicroCom::Regs prefixed=MicroCom::no, int count=0);
+    /** in/out指令 **/
+    void in(MicroCom::Regs reg, int addr=-1);
+    void out(MicroCom::Regs reg, int addr=-1);
+
+    /** 出入栈指令 **/
+    //push
+    void push(MicroCom::Regs source);
+    void push(MicroCom::Regs sBased, int tOffset, MicroCom::Regs tIndexed, MicroCom::Regs tPrefixed=MicroCom::no);
+    void push(int addr);
+    //pop
+    void pop(MicroCom::Regs target);
+    void pop(MicroCom::Regs tBased, int tOffset, MicroCom::Regs tIndexed, MicroCom::Regs tPrefixed=MicroCom::no);
+    void pop(int addr);
+
+    /** XCHG交换指令 **/
+    //to register
+    void xChg(MicroCom::Regs target, MicroCom::Regs source);
+    void xChg(MicroCom::Regs target, MicroCom::Regs sBased, int sOffset, MicroCom::Regs sIndexed=MicroCom::no, MicroCom::Regs sPrefixed=MicroCom::no);
+    void xChg(MicroCom::Regs target, int sAddr);
+
+    //to Memory
+    void xChg(int targetAddr, MicroCom::Regs source);
+    //void xChg(MicroCom::Regs tBased, int tOffset, MicroCom::Regs tPrefixed, MicroCom::Regs source);
+    //void xChg(MicroCom::Regs tBased, MicroCom::Regs tIndexed, int tOffset, MicroCom::Regs tPrefixed, MicroCom::Regs source);
+
+    /** LEA取地址有效指令 **/
+    //lea要求源操作数只能是存储单元，并且目的操作数必须是一个寄存器  (变量怎么表示呢？)
+    //void LoadEffectiveAdds();
+    void lea(MicroCom::Regs target, MicroCom::Regs sBased, int sOffset, MicroCom::Regs sIndexed=MicroCom::no);
+    void lea(MicroCom::Regs target, QString symbol);
+
+    /** LDS将双字指针送到寄存器和DS指令(Load Pointer Using DS) **/
+    void loadPointerUsingDS(MicroCom::Regs target, MicroCom::Regs sBased, int sOffset, MicroCom::Regs sIndexed=MicroCom::no);
+    void loadPointerUsingDS(MicroCom::Regs target, int sAddr);
+    void loadPointerUsingDS(MicroCom::Regs target, QString symbol);
+
+
+
+
+
+    /** 寻址指令 **/
+    int getPhyAddr(MicroCom::Regs based, int offset, MicroCom::Regs indexed= MicroCom::no, MicroCom::Regs prefixed=MicroCom::no);
+
     //连线函数，将两个引脚连接起来
     void link(Hardwares *sender, MicroCom::Pins pinS, Hardwares *receiver, MicroCom::Pins pinR);
-    bool isOdd(int i);
 
     //Input and Output
-    void in(CPUs* cp, MicroCom::Regs reg, int addr=-1);
-    void out(CPUs* cp, MicroCom::Regs reg, int addr=-1);
+
 
 private:
     Ui::MainWindow *ui;
-    CPUs* cp1;
+    CPUs* cp;
     Latchs* la1;
     Latchs* la2;
     Latchs* la3;
